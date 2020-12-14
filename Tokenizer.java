9e5fdbc63fadae27bf7a78836da73ead24638569
plb4dq <60238244+plb4dq@users.noreply.github.com>
2020-12-14 11:28:41 -0600

    Create Tokenizer.java
    added Tokenizer.java file from computer

---
+++ Tokenizer.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class Tokenizer {
+
+	static String[] doTokenize(String s) {
+		String s2 = s.replaceAll("\\s+", "");
+		String[] final = s2.split("(?<=[-+*/=()])|(?=[-+*/=()])");
+		
+		return final;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				
+		System.out.println(Arrays.toString(doTokenize("123+56*num1"))); //returns 123, +, 56, *, num1
+		System.out.println(Arrays.toString(doTokenize("(1+ 23) * 9"))); //→ returns (, 1, +, 23, ), *, 9
+		System.out.println(Arrays.toString(doTokenize("aa1= (14 - 3) *2/a23"))); //→ returns aa1, =, (, 14, -, 3, ), *, 2, /, a23
+
+	}
+	
+	
+}
